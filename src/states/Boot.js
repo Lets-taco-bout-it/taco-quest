1352c00cf3ec3b02d4be84e5397d0836b0698ab5
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-09-13 20:45:19 -0600

    world created

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,9 +1,45 @@
 import Phaser from "phaser";
 
+var guy,
+  background,
+  speed = 4;
+
 export default class extends Phaser.State {
-  preload() {}
+  constructor() {
+    super();
+    //keep track of variables here
+  }
+  preload() {
+    game.load.image("CityBG", "src/assets/CityBG.png");
+    game.load.spritesheet("guy", "src/assets/guy_sheet.png", 32, 32);
+  }
 
-  create() {}
+  create() {
+    background = game.add.tileSprite(0, 0, 1920, 1080, "CityBG");
+    background.anchor.setTo(0, 0.51);
+    background.scale.setTo(1.5, 1.5);
 
-  update() {}
+    guy = game.add.sprite(20, 525, "guy");
+    guy.scale.setTo(2, 2);
+    guy.anchor.setTo(0.5, 0.5);
+    guy.animations.add("walk", [0, 1, 2, 3, 4]);
+  }
+
+  update() {
+    //moving background
+    // background.tilePosition.x -= 2;
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      guy.scale.setTo(2, 2);
+      guy.x += speed;
+      guy.animations.play("walk", 14, true);
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      guy.scale.setTo(-2, 2);
+      guy.x -= speed;
+      guy.animations.play("walk", 14, true);
+    } else {
+      guy.animations.stop("walk");
+      guy.frame = 0;
+    }
+  }
 }
