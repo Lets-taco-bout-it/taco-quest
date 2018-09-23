7205c9ea392aafaa806140f3e0d93bc2fa2b42a3
Doug Small <dsmalz13@gmail.com>
2018-09-22 18:49:52 -0600

    added guy to intro screen

--- src/states/intro.js
+++ src/states/intro.js
@@ -11,7 +11,7 @@ var line = [];
 
 var wordIndex = 0;
 var lineIndex = 0;
-
+var guy;
 var wordDelay = 120;
 var lineDelay = 400;
 var text;
@@ -25,6 +25,7 @@ var scroll = false;
 export default class extends Phaser.State {
   preload() {
     game.load.image("CityBG", "src/assets/CityBG.png");
+    game.load.spritesheet("guy", "src/assets/guy_sheet.png", 32, 32);
   }
 
   create() {
@@ -50,6 +51,9 @@ export default class extends Phaser.State {
 
       if (background.tilePosition.y <= -551) {
         scroll = false;
+        guy = game.add.sprite(100, 525, "guy");
+        guy.scale.setTo(2, 2);
+        guy.anchor.setTo(0.5, 0.5);
         // return background.anchor.setTo(0, 0.51);
       }
     }
