1576b0f1afecd416940bde54f6177a846b84361c
Doug Small <dsmalz13@gmail.com>
2018-10-02 20:47:49 -0600

    added controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const massive = require("massive");
 const cors = require("cors");
+const controller = require("./controller");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const app = express();
