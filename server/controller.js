5157ecec36bea68583d60c06cc60da281985b613
Doug Small <dsmalz13@gmail.com>
2018-10-02 20:52:43 -0600

    post added in controller

--- server/controller.js
+++ server/controller.js
@@ -31,5 +31,41 @@ module.exports = {
         res.status(500).send({ errorMessage: "get request error!" });
         console.log(err);
       });
+  },
+  postLvl1: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { initials, score } = req.body;
+
+    dbInstance
+      .post_lvl1([initials, score])
+      .then(() => res.sendStatus(200, "All good!"))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Server Error!" });
+        console.log(err);
+      });
+  },
+  postLvl2: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { initials, score } = req.body;
+
+    dbInstance
+      .post_lvl2([initials, score])
+      .then(() => res.sendStatus(200, "All good!"))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Server Error!" });
+        console.log(err);
+      });
+  },
+  postLvl3: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { initials, score } = req.body;
+
+    dbInstance
+      .post_lvl3([initials, score])
+      .then(() => res.sendStatus(200, "All good!"))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Server Error!" });
+        console.log(err);
+      });
   }
 };
