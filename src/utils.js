13f4e36b1a2f296d4902ccd27d2543dde4492978
Doug Small <dsmalz13@gmail.com>
2018-10-20 14:36:23 -0600

    utils change

--- src/utils.js
+++ src/utils.js
@@ -8,12 +8,12 @@ export const switchState = () => {
     "Boot",
     "intro",
     "LevelONE",
+    "HighScore",
     "LevelTWO",
-    "HighScore"
+
     // "DougLevel1",
     // "MegLevel1",
     // "MaxsLevel1",
-    "MaxsLevel2",
     "HighScore2"
   ];
 
